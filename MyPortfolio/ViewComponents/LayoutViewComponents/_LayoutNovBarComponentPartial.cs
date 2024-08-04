97524bbe8a68d6e4162980723fe31eeb39b12bda
Koray Topuz <krytopuz@gmail.com>
2024-08-04 11:08:54 +0300

    .

---
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNovBarComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutNovBarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke() {  return View(); }
+	}
+}
