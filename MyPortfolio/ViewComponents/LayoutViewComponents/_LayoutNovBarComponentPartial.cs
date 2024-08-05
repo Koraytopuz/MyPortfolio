adbfef673514d299ef3669d2712ef572ea99df71
Koray Topuz <krytopuz@gmail.com>
2024-08-06 00:10:08 +0300

    ...

--- MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNovBarComponentPartial.cs
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutNovBarComponentPartial.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.ViewComponents.LayoutViewComponents
 {
 	public class _LayoutNovBarComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke() {  return View(); }
+		MyPortfolioContext context=new MyPortfolioContext();
+		public IViewComponentResult Invoke() 
+		{
+			ViewBag.toDoListCount=context.ToDoLists.Where(x=>x.Status==false).Count();
+			var values=context.ToDoLists.Where(x => x.Status==false).ToList();
+			return View(values); }
 	}
 }
